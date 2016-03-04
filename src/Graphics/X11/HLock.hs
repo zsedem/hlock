909e5352c14f487245fec454225cfc48c26f30b0
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2016-03-04 20:39:03 +0100

    currenly locks for 10 second then exits
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- src/Graphics/X11/HLock.hs
+++ src/Graphics/X11/HLock.hs
@@ -1,14 +1,20 @@
 module Graphics.X11.HLock(hlock) where
-import ClassyPrelude (MonadIO, forM, liftIO, print, (=<<))
+import ClassyPrelude
 import Data.Bits((.|.))
+import Data.Time.Clock.POSIX(getPOSIXTime)
+import Control.Concurrent (threadDelay)
+import System.Exit(exitSuccess, exitFailure)
 import qualified Graphics.X11.Xlib as X
 import qualified Graphics.X11.Xrandr as X
+import qualified Graphics.X11.Xlib.Extras as X
 
 hlock :: MonadIO io => io ()
 hlock = liftIO $ do
     dpy <- X.openDisplay ""
     let screenCount' = fromIntegral $ X.screenCount dpy
-    _locks <- forM [0..screenCount' - 1] $ lockScreen dpy
+    locks <- forM [0..screenCount' - 1] $ lockScreen dpy
+    threadDelay (10 * 1000000)
+    exitSuccess
     X.closeDisplay dpy
 
 lockScreen :: X.Display -> X.ScreenNumber -> IO LockT
@@ -18,9 +24,10 @@ lockScreen dpy scr = X.allocaSetWindowAttributes $ \windowattributes -> do
         depth = X.defaultDepth dpy scr
         displayWidth = fromIntegral $ X.displayWidth dpy scr
         displayHeight = fromIntegral $ X.displayHeight dpy scr
+    blackColor <- initColor dpy "black"
     rootWindow' <- X.rootWindow dpy scr
     X.set_background_pixel windowattributes black
-    X.set_override_redirect windowattributes False
+    X.set_override_redirect windowattributes True
     newWindow <- X.createWindow
         dpy
         rootWindow'
@@ -34,18 +41,37 @@ lockScreen dpy scr = X.allocaSetWindowAttributes $ \windowattributes -> do
         visual
         (X.cWOverrideRedirect .|. X.cWBackPixel)
         windowattributes
-    -- add cursor hiding
     X.xrrSelectInput dpy newWindow X.rrScreenChangeNotifyMask
     X.mapWindow dpy newWindow
     X.raiseWindow dpy newWindow
-    return $ Lock { screen = scr
-                  , rootWindow = rootWindow'
-                  , window = newWindow
-                  , pmap = undefined
-                  }
+    pmap' <- X.createPixmap dpy newWindow 5 10 (X.defaultDepthOfScreen (X.defaultScreenOfDisplay dpy))
+    X.grabPointer dpy rootWindow' False (X.buttonPressMask .|. X.buttonReleaseMask .|. X.pointerMotionMask)
+                  X.grabModeAsync X.grabModeAsync 0 0 X.currentTime
+
+    grabKeyboard dpy scr rootWindow'
+    X.selectInput dpy rootWindow' X.substructureNotifyMask
+    return Lock { screen = scr
+                , rootWindow = rootWindow'
+                , window = newWindow
+                }
 data LockT = Lock
     { screen :: X.ScreenNumber
     , rootWindow, window :: X.Window
-    , pmap :: X.Pixmap
+--    , pmap :: X.Pixmap
 --    , unsigned long colors[NUMCOLS]
     }
+
+initColor dpy color = do
+    let colormap = X.defaultColormap dpy (X.defaultScreen dpy)
+    (apros,real) <- X.allocNamedColor dpy colormap color
+    return apros
+
+grabKeyboard = grabKeyboard' 1000
+  where
+    grabKeyboard' try dpy scr rw | try > 0 = do
+        result<- X.grabKeyboard dpy rw True X.grabModeAsync X.grabModeAsync X.currentTime
+        unless (result == X.grabSuccess) $ do
+            threadDelay 10
+            grabKeyboard' (try - 1) dpy scr rw
+    grabKeyboard' _ _ _ _ = exitFailure
+

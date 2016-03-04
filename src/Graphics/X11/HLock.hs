3705a479ededbbe9e25f0c3155aa0e1bb19a7297
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2016-03-04 21:50:18 +0100

    refactor and make place for the password reading
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- src/Graphics/X11/HLock.hs
+++ src/Graphics/X11/HLock.hs
@@ -2,63 +2,42 @@ module Graphics.X11.HLock(hlock) where
 import ClassyPrelude
 import Data.Bits((.|.))
 import Data.Time.Clock.POSIX(getPOSIXTime)
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay, forkIO)
 import System.Exit(exitSuccess, exitFailure)
 import qualified Graphics.X11.Xlib as X
 import qualified Graphics.X11.Xrandr as X
 import qualified Graphics.X11.Xlib.Extras as X
 
-hlock :: MonadIO io => io ()
-hlock = liftIO $ do
+hlock :: MonadIO io => Bool -> io ()
+hlock justTest = liftIO $ do
     dpy <- X.openDisplay ""
-    let screenCount' = fromIntegral $ X.screenCount dpy
-    locks <- forM [0..screenCount' - 1] $ lockScreen dpy
-    threadDelay (10 * 1000000)
-    exitSuccess
+    locks <- forAllScreen dpy $ lockScreen dpy
+    when justTest $ void $ forkIO $ do
+        threadDelay (15 * 1000000)
+        putStrLn "Exited after 15 sec because you set to just Testing"
+        exitFailure
+    readPassword
     X.closeDisplay dpy
+    exitSuccess
 
 lockScreen :: X.Display -> X.ScreenNumber -> IO LockT
 lockScreen dpy scr = X.allocaSetWindowAttributes $ \windowattributes -> do
-    let black = X.blackPixel dpy scr
-        visual = X.defaultVisual dpy scr
-        depth = X.defaultDepth dpy scr
-        displayWidth = fromIntegral $ X.displayWidth dpy scr
-        displayHeight = fromIntegral $ X.displayHeight dpy scr
     blackColor <- initColor dpy "black"
     rootWindow' <- X.rootWindow dpy scr
-    X.set_background_pixel windowattributes black
-    X.set_override_redirect windowattributes True
-    newWindow <- X.createWindow
-        dpy
-        rootWindow'
-        0
-        0
-        displayWidth
-        displayHeight
-        0
-        depth
-        X.copyFromParent
-        visual
-        (X.cWOverrideRedirect .|. X.cWBackPixel)
-        windowattributes
+    newWindow <- createBlackWindow dpy scr rootWindow'
     X.xrrSelectInput dpy newWindow X.rrScreenChangeNotifyMask
-    X.mapWindow dpy newWindow
-    X.raiseWindow dpy newWindow
-    pmap' <- X.createPixmap dpy newWindow 5 10 (X.defaultDepthOfScreen (X.defaultScreenOfDisplay dpy))
     X.grabPointer dpy rootWindow' False (X.buttonPressMask .|. X.buttonReleaseMask .|. X.pointerMotionMask)
                   X.grabModeAsync X.grabModeAsync 0 0 X.currentTime
 
     grabKeyboard dpy scr rootWindow'
-    X.selectInput dpy rootWindow' X.substructureNotifyMask
     return Lock { screen = scr
                 , rootWindow = rootWindow'
                 , window = newWindow
                 }
+
 data LockT = Lock
     { screen :: X.ScreenNumber
     , rootWindow, window :: X.Window
---    , pmap :: X.Pixmap
---    , unsigned long colors[NUMCOLS]
     }
 
 initColor dpy color = do
@@ -66,12 +45,41 @@ initColor dpy color = do
     (apros,real) <- X.allocNamedColor dpy colormap color
     return apros
 
-grabKeyboard = grabKeyboard' 1000
+grabKeyboard dpy scr rw = do
+    grabKeyboard' 100 dpy scr rw
+    X.selectInput dpy rw X.substructureNotifyMask
   where
     grabKeyboard' try dpy scr rw | try > 0 = do
         result<- X.grabKeyboard dpy rw True X.grabModeAsync X.grabModeAsync X.currentTime
         unless (result == X.grabSuccess) $ do
-            threadDelay 10
+            threadDelay 100
             grabKeyboard' (try - 1) dpy scr rw
     grabKeyboard' _ _ _ _ = exitFailure
+
+
+createBlackWindow dpy scr rw = X.allocaSetWindowAttributes $ \windowattributes -> do
+        X.set_background_pixel windowattributes $
+            X.blackPixel dpy scr
+        X.set_override_redirect windowattributes True
+        window <- X.createWindow
+            dpy
+            rw
+            0
+            0
+            (fromIntegral $ X.displayWidth dpy scr)
+            (fromIntegral $ X.displayHeight dpy scr)
+            0
+            (X.defaultDepth dpy scr)
+            X.copyFromParent
+            (X.defaultVisual dpy scr)
+            (X.cWOverrideRedirect .|. X.cWBackPixel)
+            windowattributes
+        X.mapWindow dpy window
+        X.raiseWindow dpy window
+        return window
+
+forAllScreen dpy = forM [0..screenCount' - 1]
+  where screenCount' = fromIntegral $ X.screenCount dpy
+
+readPassword = undefined
 

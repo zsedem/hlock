90f7b00deece74b46aad2341ee25f841e34d6eb7
Zsigmond Adam Oliver <zsedem@gmail.com>
2016-03-06 08:26:22 +0100

    refactor the whole thing to have no warning with Wall
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- src/Graphics/X11/HLock.hs
+++ src/Graphics/X11/HLock.hs
@@ -1,7 +1,6 @@
 module Graphics.X11.HLock(hlock) where
 import ClassyPrelude
 import Data.Bits((.|.))
-import Data.Time.Clock.POSIX(getPOSIXTime)
 import Control.Concurrent (threadDelay, forkIO)
 import System.Exit(exitSuccess, exitFailure)
 import qualified Graphics.X11.Xlib as X
@@ -13,23 +12,24 @@ hlock justTest = liftIO $ do
     dpy <- X.openDisplay ""
     locks <- forAllScreen dpy $ lockScreen dpy
     when justTest $ void $ forkIO $ do
-        threadDelay (15 * 1000000)
+        threadDelay (25 * 1000000)
         putStrLn "Exited after 15 sec because you set to just Testing"
         exitFailure
     readPassword
+    forM_ locks $
+        unlockScreen dpy
     X.closeDisplay dpy
     exitSuccess
+  where
+    unlockScreen _ _ = return ()
 
 lockScreen :: X.Display -> X.ScreenNumber -> IO LockT
-lockScreen dpy scr = X.allocaSetWindowAttributes $ \windowattributes -> do
-    blackColor <- initColor dpy "black"
+lockScreen dpy scr = do
     rootWindow' <- X.rootWindow dpy scr
     newWindow <- createBlackWindow dpy scr rootWindow'
     X.xrrSelectInput dpy newWindow X.rrScreenChangeNotifyMask
-    X.grabPointer dpy rootWindow' False (X.buttonPressMask .|. X.buttonReleaseMask .|. X.pointerMotionMask)
-                  X.grabModeAsync X.grabModeAsync 0 0 X.currentTime
-
-    grabKeyboard dpy scr rootWindow'
+    grabPointer dpy rootWindow'
+    grabKeyboard dpy rootWindow'
     return Lock { screen = scr
                 , rootWindow = rootWindow'
                 , window = newWindow
@@ -40,30 +40,35 @@ data LockT = Lock
     , rootWindow, window :: X.Window
     }
 
-initColor dpy color = do
-    let colormap = X.defaultColormap dpy (X.defaultScreen dpy)
-    (apros,real) <- X.allocNamedColor dpy colormap color
-    return apros
 
-grabKeyboard dpy scr rw = do
-    grabKeyboard' 100 dpy scr rw
+grabPointer :: X.Display -> X.Window -> IO ()
+grabPointer dpy rootWindow' = void $
+    X.grabPointer dpy rootWindow' False (X.buttonPressMask .|. X.buttonReleaseMask .|. X.pointerMotionMask)
+                  X.grabModeAsync X.grabModeAsync 0 0 X.currentTime
+
+
+grabKeyboard :: X.Display -> X.Window -> IO ()
+grabKeyboard dpy rw = do
+    grabKeyboard' 100
     X.selectInput dpy rw X.substructureNotifyMask
   where
-    grabKeyboard' try dpy scr rw | try > 0 = do
+    grabKeyboard' :: Int -> IO ()
+    grabKeyboard' tries | tries > 0 = do
         result<- X.grabKeyboard dpy rw True X.grabModeAsync X.grabModeAsync X.currentTime
         unless (result == X.grabSuccess) $ do
             threadDelay 100
-            grabKeyboard' (try - 1) dpy scr rw
-    grabKeyboard' _ _ _ _ = exitFailure
+            grabKeyboard' (tries - 1)
+    grabKeyboard' _ = exitFailure
 
 
-createBlackWindow dpy scr rw = X.allocaSetWindowAttributes $ \windowattributes -> do
+createBlackWindow :: X.Display -> X.ScreenNumber -> X.Window -> IO X.Window
+createBlackWindow dpy scr rootWindow' = X.allocaSetWindowAttributes $ \windowattributes -> do
         X.set_background_pixel windowattributes $
             X.blackPixel dpy scr
         X.set_override_redirect windowattributes True
-        window <- X.createWindow
+        newWindow <- X.createWindow
             dpy
-            rw
+            rootWindow'
             0
             0
             (fromIntegral $ X.displayWidth dpy scr)
@@ -74,12 +79,15 @@ createBlackWindow dpy scr rw = X.allocaSetWindowAttributes $ \windowattributes -
             (X.defaultVisual dpy scr)
             (X.cWOverrideRedirect .|. X.cWBackPixel)
             windowattributes
-        X.mapWindow dpy window
-        X.raiseWindow dpy window
-        return window
+        X.mapWindow dpy newWindow
+        X.raiseWindow dpy newWindow
+        return newWindow
 
+forAllScreen :: X.Display -> (X.ScreenNumber -> IO b) -> IO [b]
 forAllScreen dpy = forM [0..screenCount' - 1]
   where screenCount' = fromIntegral $ X.screenCount dpy
 
-readPassword = undefined
+-- TODO better name here
+readPassword :: IO ()
+readPassword = error "The red shirts are coming"
 

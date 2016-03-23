c0b9c437930e3be143a8724e7aab15352ffaa966
Zsigmond Adam Oliver <zsedem@gmail.com>
2016-03-23 18:50:20 +0100

    fix locking on cases when we are not in a login shell
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- src/System/Posix/User/Password.hs
+++ src/System/Posix/User/Password.hs
@@ -9,6 +9,8 @@ import System.Unix.Crypt
 checkPassword :: String -> IO Bool
 checkPassword password = do
     setEffectiveUserID 0
-    pws <- sUserPassword <$> (getLoginName >>= getSUserEntryForName)
+    pws <- sUserPassword <$> (getRealUserName >>= getSUserEntryForName)
     (pws==) <$> crypt password pws
-
+  where
+    getRealUserName :: IO String
+    getRealUserName = userName <$> (getRealUserID >>= getUserEntryForID)

5086feda41f02d65133b7bb30d15acf14f3b069a
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2016-03-29 22:32:05 +0200

    add command arg parsing
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- Main.hs
+++ Main.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main(main) where
 import Graphics.X11.HLock(hlock)
+import System.Console.CmdArgs
+data Arguments = Arguments
+    {}
+  deriving (Show, Data, Typeable)
+
+arguments :: Arguments
+arguments = Arguments
+  {} &= summary "hlock v0.1.1.0 Simple Locker for Xorg display"
 
 main :: IO ()
-main = hlock
+main = do
+    _ <- cmdArgs arguments
+    hlock
